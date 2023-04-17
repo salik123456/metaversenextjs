e1f57311d3ca36565fd56000a5fd85aff6a13569
salik123456 <salikmohammed00@gmail.com>
2023-04-17 15:44:40 +0500

    first commit

---
+++ app/head.js
@@ -0,0 +1,16 @@
+const Head = () => (
+  <>
+    <title>Metaversus</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+    <meta property="og:image" content="/cover.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:title" content="Metaversus" />
+<meta property="og:description" content="Metaverse is a new thing in the future" />
+<meta property="og:type" content="website" />
+
+  </>
+);
+
+export default Head;
